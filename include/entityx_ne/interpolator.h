69252c5a6bd44eee257c949d6c3e60af5a7a6741
Israël Hallé <israel.halle@shopify.com>
2014-07-27 17:28:34 -0400

    Add interpolation system

---
+++ include/entityx_ne/interpolator.h
@@ -0,0 +1,53 @@
+#ifndef ENTITYX_NE_INTERPORLATOR_H
+#define ENTITYX_NE_INTERPORLATOR_H
+
+#include <entityx/entityx.h>
+
+namespace entityx {
+  namespace ne {
+    template <typename C>
+    struct FramedComponent : public entityx::Component < FramedComponent<C> > {
+      C base;
+      C renderable;
+    };
+
+    template <typename I, typename Type>
+    class InterpolateSystem : public entityx::System < InterpolateSystem<I, Type> > {
+    public:
+      void update(entityx::EntityManager &entities, entityx::EventManager &events, double dt) {
+        Type::Handle current;
+        FramedComponent<Type>::Handle frameComponent;
+
+        for (Entity e : entities.entities_with_components(current, frameComponent)) {
+          interpolator_.interpolate(frameComponent->base, *current.get(), frameComponent->renderable, dt / frameLength_);
+        }
+      }
+
+    private:
+      I interpolator_;
+      double frameLength_ = 1.0f;
+    };
+
+    template<typename Type>
+    struct CopyBaseSystem : public entityx::System < CopyBaseSystem<Type> > {
+      void update(entityx::EntityManager &entities, entityx::EventManager &events, double dt) {
+        Type::Handle current;
+        FramedComponent<Type>::Handle frameComponent;
+
+        for (Entity e : entities.entities_with_components(current, frameComponent)) {
+          frameComponent->base = *current.get();
+        }
+      }
+    };
+
+    template <typename I, typename T>
+    struct Interpolator {
+      typedef InterpolateSystem<I, T> System;
+      typedef CopyBaseSystem<T> BaseSystem;
+      virtual void interpolate(const T& base, const T& current, T& interpolated, double t)  {}
+    };
+
+  }
+}
+
+#endif
---
+++ test/interpolator_test.cpp
@@ -0,0 +1,119 @@
+#include <gtest/gtest.h>
+#include <entityx/entityx.h>
+#include <entityx_ne/interpolator.h>
+
+struct TestComponent : public entityx::Component < TestComponent > {
+  int state = 0;
+};
+
+struct TestInterpolator : public entityx::ne::Interpolator< TestInterpolator, TestComponent > {
+  void interpolate(const TestComponent& base, const TestComponent& current, TestComponent& interpolated, double t) override {
+    interpolated.state = base.state * (1 - t) + current.state * t + 0.5f;
+  }
+};
+
+struct TestSystem : public entityx::System < TestSystem > {
+  void update(entityx::EntityManager &es, entityx::EventManager &events, double dt) override {
+    TestComponent::Handle c;
+    for (entityx::Entity entity : es.entities_with_components(c)) {
+      c->state += (10 * dt) + 0.5f;
+    }
+  }
+};
+
+class InteroplatorTest : public testing::Test {
+protected:
+  virtual void SetUp() {
+    instance.systems.add<TestSystem>();
+    instance.systems.add<TestInterpolator::BaseSystem>();
+    instance.systems.add<TestInterpolator::System>();
+    instance.systems.configure();
+
+    local_entity = instance.entities.create();
+    local_entity.assign<TestComponent>();
+    local_entity.assign<entityx::ne::FramedComponent<TestComponent>>();
+  }
+
+  void update() {
+    instance.systems.update<TestInterpolator::BaseSystem>(1.f);
+    instance.systems.update<TestSystem>(1.f);
+  }
+
+  void renderUpdate(double dt) {
+    instance.systems.update<TestInterpolator::System>(dt);
+  }
+  
+  entityx::EntityX instance;
+  entityx::Entity local_entity;
+};
+
+TEST_F(InteroplatorTest, FixtureTest) {
+  TestComponent::Handle c = local_entity.component<TestComponent>();
+  EXPECT_EQ(0, c->state);
+  update();
+  EXPECT_EQ(10, c->state);
+  update();
+  EXPECT_EQ(20, c->state);
+}
+
+TEST_F(InteroplatorTest, BaseComponent) {
+  auto c = local_entity.component<TestComponent>();
+  auto framed_c = local_entity.component<entityx::ne::FramedComponent<TestComponent>>();
+
+  c->state = 1;
+  EXPECT_EQ(1, c->state);
+  EXPECT_EQ(0, framed_c->base.state);
+
+  instance.systems.update<TestInterpolator::BaseSystem>(1.f);
+  EXPECT_EQ(1, c->state);
+  EXPECT_EQ(1, framed_c->base.state);
+
+  c->state = 2;
+  EXPECT_EQ(2, c->state);
+  EXPECT_EQ(1, framed_c->base.state);
+}
+
+TEST_F(InteroplatorTest, CopyBaseSystem) {
+  auto c = local_entity.component<TestComponent>();
+  auto framed_c = local_entity.component<entityx::ne::FramedComponent<TestComponent>>();
+
+  EXPECT_EQ(0, c->state);
+  EXPECT_EQ(0, framed_c->base.state);
+
+  update();
+  EXPECT_EQ(10, c->state);
+  EXPECT_EQ(0, framed_c->base.state);
+
+  update();
+  EXPECT_EQ(20, c->state);
+  EXPECT_EQ(10, framed_c->base.state);
+}
+
+TEST_F(InteroplatorTest, InterpolateSystem) {
+  auto c = local_entity.component<TestComponent>();
+  auto framed_c = local_entity.component<entityx::ne::FramedComponent<TestComponent>>();
+
+  EXPECT_EQ(0, c->state);
+  EXPECT_EQ(0, framed_c->renderable.state);
+
+  update();
+  EXPECT_EQ(10, c->state);
+  EXPECT_EQ(0, framed_c->renderable.state);
+
+  renderUpdate(0.5f);
+  EXPECT_EQ(10, c->state);
+  EXPECT_EQ(5, framed_c->renderable.state);
+
+  renderUpdate(0.6f);
+  EXPECT_EQ(10, c->state);
+  EXPECT_EQ(6, framed_c->renderable.state);
+
+  update();
+  renderUpdate(0.0f);
+  EXPECT_EQ(20, c->state);
+  EXPECT_EQ(10, framed_c->renderable.state);
+
+  renderUpdate(1.0f);
+  EXPECT_EQ(20, c->state);
+  EXPECT_EQ(20, framed_c->renderable.state);
+}
